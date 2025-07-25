4e4a186d3afb0b799764e91b446db9465ec370bc
satheeshari3 <satheeshari2022@gmail.com>
2025-07-25 12:45:07 +0530

    Replaced rule-based classifier with ML model using TF-IDF + Logistic Regression

--- app/core/nlp_classifier.py
+++ app/core/nlp_classifier.py
@@ -1,7 +1,12 @@
+# app/core/nlp_classifier.py
+
+import joblib
+import os
+
+# Load model only once at module level
+model_path = os.path.join(os.path.dirname(__file__), "../../model/ticket_classifier.joblib")
+model = joblib.load(model_path)
+
 def classify_ticket(query: str) -> str:
-    if "payment" in query.lower():
-        return "billing"
-    elif "error" in query.lower() or "bug" in query.lower():
-        return "technical"
-    else:
-        return "general"
+    prediction = model.predict([query])  # Query must be in a list
+    return prediction[0]
